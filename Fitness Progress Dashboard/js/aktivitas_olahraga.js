a36da3f4ef2451f89e0af37364ce4aaccd168a0a
RyanArdyan <ryanardyan437@gmail.com>
2024-12-02 13:57:54 +0700

    Memperbarui Fitness Progress Dashboard

--- Fitness Progress Dashboard/js/aktivitas_olahraga.js
+++ Fitness Progress Dashboard/js/aktivitas_olahraga.js
@@ -31,33 +31,50 @@ $(document).ready(function() {
         $('#result').html(`<h2>Kalori Terbakar: ${calories.toFixed(2)} kalori</h2>`);
 
         // Menyiapkan data untuk Chart.js
+        // panggil #myChart, index ke 0 lalu ambil konteks nya dan gunakan 2 dimensi
         const ctx = $('#myChart')[0].getContext('2d');
 
+        // jika ada my chart maka
         if (myChart) {
+            // hapus chart nya karena chart kedua harus menghapus chart pertama
             myChart.destroy();
         }
 
+        // inisialisasi chart, ctx berisi dapatkan konteks
         myChart = new Chart(ctx, {
+            // tipe nya bar
             type: 'bar',
+            // berisi data-data
             data: {
+                // label-label nya
                 labels: ['Kalori Lari', 'Kalori Bersepeda'],
                 datasets: [{
                     label: 'Kalori Terbakar',
+                    // jika data aktivitas nya lari maka kalori nya 0 dan begitu juga bersepeda
                     data: [activity === 'lari' ? calories : 0, activity === 'bersepeda' ? calories : 0],
+                    // latar belakang
                     backgroundColor: [
+                        // latar belakang untuk Kalori Lari
                         'rgba(75, 192, 192, 0.5)',
+                        // latar belakang untuk Kalori bersepeda
                         'rgba(153, 102, 255, 0.5)'
                     ],
                     borderColor: [
+                        // warna bingkai untuk kalori lari
                         'rgba(75, 192, 192, 1)',
+                        // warna bingkai untuk kalori bersepeda
                         'rgba(153, 102, 255, 1)'
                     ],
                     borderWidth: 1
                 }]
             },
+            //  Ini adalah objek yang berisi pengaturan untuk grafik. Di dalam objek ini, Anda dapat menentukan berbagai opsi untuk mengontrol tampilan dan perilaku grafik.
             options: {
+                // Ini adalah objek yang digunakan untuk mengkonfigurasi sumbu (axes) grafik. Dalam konteks ini, kita berbicara tentang sumbu y (vertikal) dari grafik.
                 scales: {
+                    // Ini merujuk pada sumbu y dari grafik. Anda dapat mengkonfigurasi berbagai pengaturan untuk sumbu ini, seperti rentang nilai, label, dan lainnya.
                     y: {
+                        //  Ini adalah opsi spesifik untuk sumbu y. Ketika diatur ke true, ini memastikan bahwa sumbu y akan dimulai dari angka 0. 
                         beginAtZero: true
                     }
                 }
